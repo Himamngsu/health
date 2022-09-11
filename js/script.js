cda2176ed256f20ee8774f567369302ad1ebd69e
Himangsu <91278808+Himamngsu@users.noreply.github.com>
2022-09-11 13:09:02 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(function(){
+    'use strict'
+
+    $('.banner_slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: false,
+    });
+
+
+
+
+
+})
